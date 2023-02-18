4a3e1bfe01dee649e23952bd120876948389616b
Rutvik_D <rutvikprajapati9999@gmail.com>
2023-02-18 15:42:10 +0530

    ''

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -6,42 +6,45 @@ $(function () {
   // =====================================
   var chart = {
     series: [
-      {
-        name: "Eanings this month",
-        data: [1.5, 2.7, 2.2, 3.6, 1.5, 1.0],
-      },
-      {
-        name: "Expense this month",
-        data: [-1.8, -1.1, -2.5, -1.5, -0.6, -1.8],
-      },
+      { name: "Earnings this month:", data: [355, 390, 300, 350, 390, 180, 355, 390] },
+      { name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
     ],
+
     chart: {
-      toolbar: {
-        show: false,
-      },
       type: "bar",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
+      height: 345,
+      offsetX: -15,
+      toolbar: { show: true },
       foreColor: "#adb0bb",
-      height: 320,
-      stacked: true,
+      fontFamily: 'inherit',
+      sparkline: { enabled: false },
     },
+
+
     colors: ["#5D87FF", "#49BEFF"],
+
+
     plotOptions: {
       bar: {
         horizontal: false,
-        barHeight: "60%",
-        columnWidth: "20%",
+        columnWidth: "35%",
         borderRadius: [6],
         borderRadiusApplication: 'end',
         borderRadiusWhenStacked: 'all'
       },
     },
+    markers: { size: 0 },
+
     dataLabels: {
       enabled: false,
     },
+
+
     legend: {
       show: false,
     },
+
+
     grid: {
       borderColor: "rgba(0,0,0,0.1)",
       strokeDashArray: 3,
@@ -51,34 +54,51 @@ $(function () {
         },
       },
     },
-    yaxis: {
-      min: -5,
-      max: 5,
-      title: {
-        // text: 'Age',
-      },
-    },
-    xaxis: {
-      axisBorder: {
-        show: false,
-      },
-      categories: [
-        "16/08",
-        "17/08",
-        "18/08",
-        "19/08",
-        "20/08",
-        "21/08",
-        "22/08",
-      ],
 
+    xaxis: {
+      type: "category",
+      categories: ["16/08", "17/08", "18/08", "19/08", "20/08", "21/08", "22/08", "23/08"],
+      labels: {
+        style: { cssClass: "grey--text lighten-2--text fill-color" },
+      },
     },
+
+
     yaxis: {
+      show: true,
+      min: 0,
+      max: 400,
       tickAmount: 4,
+      labels: {
+        style: {
+          cssClass: "grey--text lighten-2--text fill-color",
+        },
+      },
     },
-    tooltip: {
-      theme: "dark",
+    stroke: {
+      show: true,
+      width: 3,
+      lineCap: "butt",
+      colors: ["transparent"],
     },
+
+
+    tooltip: { theme: "light" },
+
+    responsive: [
+      {
+        breakpoint: 600,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 3,
+            }
+          },
+        }
+      }
+    ]
+
+
   };
 
   var chart = new ApexCharts(document.querySelector("#chart"), chart);

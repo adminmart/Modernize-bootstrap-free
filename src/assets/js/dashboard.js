9d3c1adbe16ee41672f7beacb55e49c591b90673
Rutvik_D <rutvikprajapati9999@gmail.com>
2023-02-18 12:56:58 +0530

    ''

---
+++ src/assets/js/dashboard.js
@@ -0,0 +1,191 @@
+$(function () {
+
+
+  // =====================================
+  // Profit
+  // =====================================
+  var chart = {
+    series: [
+      {
+        name: "Eanings this month",
+        data: [1.5, 2.7, 2.2, 3.6, 1.5, 1.0],
+      },
+      {
+        name: "Expense this month",
+        data: [-1.8, -1.1, -2.5, -1.5, -0.6, -1.8],
+      },
+    ],
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      type: "bar",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
+      foreColor: "#adb0bb",
+      height: 320,
+      stacked: true,
+    },
+    colors: ["#5D87FF", "#49BEFF"],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        barHeight: "60%",
+        columnWidth: "20%",
+        borderRadius: [6],
+        borderRadiusApplication: 'end',
+        borderRadiusWhenStacked: 'all'
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    grid: {
+      borderColor: "rgba(0,0,0,0.1)",
+      strokeDashArray: 3,
+      xaxis: {
+        lines: {
+          show: false,
+        },
+      },
+    },
+    yaxis: {
+      min: -5,
+      max: 5,
+      title: {
+        // text: 'Age',
+      },
+    },
+    xaxis: {
+      axisBorder: {
+        show: false,
+      },
+      categories: [
+        "16/08",
+        "17/08",
+        "18/08",
+        "19/08",
+        "20/08",
+        "21/08",
+        "22/08",
+      ],
+
+    },
+    yaxis: {
+      tickAmount: 4,
+    },
+    tooltip: {
+      theme: "dark",
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), chart);
+  chart.render();
+
+
+  // =====================================
+  // Breakup
+  // =====================================
+  var breakup = {
+    color: "#adb5bd",
+    series: [38, 40, 25],
+    labels: ["2022", "2021", "2020"],
+    chart: {
+      width: 180,
+      type: "donut",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
+      foreColor: "#adb0bb",
+    },
+    plotOptions: {
+      pie: {
+        startAngle: 0,
+        endAngle: 360,
+        donut: {
+          size: '75%',
+        },
+      },
+    },
+    stroke: {
+      show: false,
+    },
+
+    dataLabels: {
+      enabled: false,
+    },
+
+    legend: {
+      show: false,
+    },
+    colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
+
+    responsive: [
+      {
+        breakpoint: 991,
+        options: {
+          chart: {
+            width: 150,
+          },
+        },
+      },
+    ],
+    tooltip: {
+      theme: "dark",
+      fillSeriesColor: false,
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
+  chart.render();
+
+
+
+  // =====================================
+  // Earning
+  // =====================================
+  var earning = {
+    chart: {
+      id: "sparkline3",
+      type: "area",
+      height: 60,
+      sparkline: {
+        enabled: true,
+      },
+      group: "sparklines",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
+      foreColor: "#adb0bb",
+    },
+    series: [
+      {
+        name: "Earnings",
+        color: "#49BEFF",
+        data: [25, 66, 20, 40, 12, 58, 20],
+      },
+    ],
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    fill: {
+      colors: ["#f3feff"],
+      type: "solid",
+      opacity: 0.05,
+    },
+
+    markers: {
+      size: 0,
+    },
+    tooltip: {
+      theme: "dark",
+      fixed: {
+        enabled: true,
+        position: "right",
+      },
+      x: {
+        show: false,
+      },
+    },
+  };
+  new ApexCharts(document.querySelector("#earning"), earning).render();
+})
